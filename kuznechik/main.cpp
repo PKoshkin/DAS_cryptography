9f0c702dc262a633b609f83275759109c36bc75e
Pavel Koshkin <koshkin.pasha@gmail.com>
2017-11-18 12:43:12 +0300

    Cosmetic changes.

--- kuznechik/main.cpp
+++ kuznechik/main.cpp
@@ -8,9 +8,7 @@
 
 
 int main(int argc, char** argv) {
-    if (argc != 3) {
-        std::cout << "Wrong args number!" << std::endl;
-    } else {
+    if (argc == 3) {
         std::bitset<128> a(argv[2]);
         auto _a = get_parts(a);
         std::deque<std::bitset<8>> _res;
@@ -22,24 +20,23 @@ int main(int argc, char** argv) {
             std::cout << "Wrong args format!" << std::endl;
         }
         std::cout << concatinate(_res) << std::endl;
+    } else if (argc == 1) {
+        std::bitset<128> key(std::string(128, '1'));
+        std::bitset<128> block(std::string(128, '0'));
+        auto splited_block = get_parts(block);
+        auto splited_key = get_parts(key);
+        std::cout << block.to_string() << std::endl;
+
+        auto lsx = L(S(X(splited_key, splited_block)));
+        std::bitset<128> result = concatinate(lsx);
+        std::cout << result.to_string() << std::endl;
+
+        auto splited_result = get_parts(result);
+        auto input = X(splited_key, S_inverse(L_inverse(splited_result)));
+        std::bitset<128> input_block = concatinate(input);
+        std::cout << input_block.to_string() << std::endl;
+    } else {
+        std::cout << "Wrong arguments number!" << std::endl;
     }
-
-
-/* lsx test:
-    std::bitset<128> key(std::string(128, '1'));
-    std::bitset<128> block(std::string(128, '0'));
-    auto splited_block = get_parts(block);
-    auto splited_key = get_parts(key);
-    std::cout << block.to_string() << std::endl;
-
-    auto lsx = L(S(X(splited_key, splited_block)));
-    std::bitset<128> result = concatinate(lsx);
-    std::cout << result.to_string() << std::endl;
-
-    auto splited_result = get_parts(result);
-    auto input = X(splited_key, S_inverse(L_inverse(splited_result)));
-    std::bitset<128> input_block = concatinate(input);
-    std::cout << input_block.to_string() << std::endl;
-*/
     return 0;
 }
