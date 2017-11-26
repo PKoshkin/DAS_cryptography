30458111b2107de9cbd9e1e4eab70c288d8e0f2e
Pavel Koshkin <koshkin.pasha@gmail.com>
2017-11-27 00:23:26 +0300

    Uglify. Should work faster.

--- kuznechik/key.cpp
+++ kuznechik/key.cpp
@@ -5,9 +5,22 @@ std::pair<Block, Block> F(const Block k, const Block a_1, const Block a_0, const
     Block a_1_copy;
     std::copy(a_1.begin(), a_1.end(), a_1_copy.begin());
     Block lsx = LSX(k, a_1_copy, multiply_data);
-    for (std::size_t i = 0; i < 16; ++i) {
-        lsx[i] ^= a_0[i];
-    }
+    lsx[0] ^= a_0[0];
+    lsx[1] ^= a_0[1];
+    lsx[2] ^= a_0[2];
+    lsx[3] ^= a_0[3];
+    lsx[4] ^= a_0[4];
+    lsx[5] ^= a_0[5];
+    lsx[6] ^= a_0[6];
+    lsx[7] ^= a_0[7];
+    lsx[8] ^= a_0[8];
+    lsx[9] ^= a_0[9];
+    lsx[10] ^= a_0[10];
+    lsx[11] ^= a_0[11];
+    lsx[12] ^= a_0[12];
+    lsx[13] ^= a_0[13];
+    lsx[14] ^= a_0[14];
+    lsx[15] ^= a_0[15];
     return std::make_pair(lsx, a_1);
 }
 
@@ -18,17 +31,36 @@ std::array<Block, 10> get_keys(const Key key, const MultiplyData& multiply_data)
     std::copy_n(key.begin() + 16, 16, result[0].begin());
     std::copy_n(key.begin(), 16, result[1].begin());
 
-    std::size_t counter = 2;
     Block constant = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    for (std::size_t i = 1; i <= 4; ++i) {
-        std::pair<Block, Block> keys = std::make_pair(result[2 * i - 2], result[2 * i - 1]);
-        for (std::size_t j = 8 * (i - 1) + 1; j <= 8 * (i - 1) + 8; ++j) {
-            constant[0] = j;
-            keys = F(L(constant, multiply_data), keys.first, keys.second, multiply_data);
-        }
-        result[counter++] = keys.first;
-        result[counter++] = keys.second;
+
+    std::pair<Block, Block> keys = std::make_pair(result[0], result[1]);
+    for (std::size_t j = 1; j <= 8; ++j) {
+        constant[0] = j;
+        keys = F(L(constant, multiply_data), keys.first, keys.second, multiply_data);
     }
+    result[2] = keys.first;
+    result[3] = keys.second;
+
+    for (std::size_t j = 9; j <= 16; ++j) {
+        constant[0] = j;
+        keys = F(L(constant, multiply_data), keys.first, keys.second, multiply_data);
+    }
+    result[4] = keys.first;
+    result[5] = keys.second;
+
+    for (std::size_t j = 17; j <= 24; ++j) {
+        constant[0] = j;
+        keys = F(L(constant, multiply_data), keys.first, keys.second, multiply_data);
+    }
+    result[6] = keys.first;
+    result[7] = keys.second;
+
+    for (std::size_t j = 25; j <= 32; ++j) {
+        constant[0] = j;
+        keys = F(L(constant, multiply_data), keys.first, keys.second, multiply_data);
+    }
+    result[8] = keys.first;
+    result[9] = keys.second;
 
     return result;
 }
--- kuznechik/kuznechik.cpp
+++ kuznechik/kuznechik.cpp
@@ -3,16 +3,28 @@
 
 Block encrypt(const Key key, Block block, const MultiplyData& multiply_data) {
     std::array<Block, 10> keys = get_keys(key, multiply_data);
-    for (std::size_t i = 0; i < 9; ++i) {
-        block = LSX(keys[i], block, multiply_data);
-    }
+    block = LSX(keys[0], block, multiply_data);
+    block = LSX(keys[1], block, multiply_data);
+    block = LSX(keys[2], block, multiply_data);
+    block = LSX(keys[3], block, multiply_data);
+    block = LSX(keys[4], block, multiply_data);
+    block = LSX(keys[5], block, multiply_data);
+    block = LSX(keys[6], block, multiply_data);
+    block = LSX(keys[7], block, multiply_data);
+    block = LSX(keys[8], block, multiply_data);
     return X(keys[9], block);
 }
 
 Block decrypt(const Key key, Block block, const MultiplyData& multiply_data) {
     std::array<Block, 10> keys = get_keys(key, multiply_data);
-    for (int i = 9; i >= 1; --i) {
-        block = LSX_inverse(keys[i], block, multiply_data);
-    }
+    block = LSX_inverse(keys[9], block, multiply_data);
+    block = LSX_inverse(keys[8], block, multiply_data);
+    block = LSX_inverse(keys[7], block, multiply_data);
+    block = LSX_inverse(keys[6], block, multiply_data);
+    block = LSX_inverse(keys[5], block, multiply_data);
+    block = LSX_inverse(keys[4], block, multiply_data);
+    block = LSX_inverse(keys[3], block, multiply_data);
+    block = LSX_inverse(keys[2], block, multiply_data);
+    block = LSX_inverse(keys[1], block, multiply_data);
     return X(keys[0], block);
 }
